2f56c8fcf7487131cbaacd7d6480218fc64d8d7b
Sajid Ahamed <sajid13673@gmail.com>
2025-03-27 14:49:51 +0530

    feat: add filtering for members using request parameters in 'Get All Members' endpoint

--- domain/Services/MemberService.php
+++ domain/Services/MemberService.php
@@ -18,8 +18,14 @@ class MemberService
     {
         try {
             $orderBy = $request->order ? $request->order : 'created_at-desc';
+            $search_term = $request->search_term;
             list($field, $direction) = explode('-', $orderBy);
-            $members = $this->member->with('user')->orderBy($field, $direction)->paginate($request->per_page);
+            $query = $this->member->with('user')->orderBy($field, $direction);
+            if ($search_term) {
+                $query->where('id', 'like', '%' . $search_term . '%')
+                    ->orWhere('name', 'like', '%' . $search_term . '%');
+            };
+            $members = $query->paginate($request->per_page);
             return response()->json(["status" => true, "data" => $members], 200);
         } catch (\Exception $e) {
             return response()->json(["status" => false, "msg" => $e->getMessage()], 500);
@@ -28,10 +34,10 @@ class MemberService
     public function store($request)
     {
         try {
-            $user = $this->member->user()->create(["email"=>$request->email, "password"=>bcrypt($request->password)]);
+            $user = $this->member->user()->create(["email" => $request->email, "password" => bcrypt($request->password)]);
             $data = $request->all();
             $member = $user->member()->create($data);
-            if($file = $request->file('image')){
+            if ($file = $request->file('image')) {
                 $saved_image = $this->uploads($file, "public/uploads");
                 $image_data = ["name" => $saved_image['fileName'], "path" => "storage/uploads/"];
                 $member->images()->create($image_data);
@@ -45,7 +51,7 @@ class MemberService
     {
         try {
             $member = $this->member->find($memberId);
-            if($request->borrowing){
+            if ($request->borrowing) {
                 $borrowings = $this->member->find($memberId)->borrowing()->with('book')->paginate($request->per_page);
                 $member->borrowing = $borrowings;
                 // $member = $this->member->with('borrowing.book')->find($memberId);
@@ -61,10 +67,10 @@ class MemberService
         try {
             $member = $this->member->find($id);
             $member->update($request->all());
-            if($file = $request->file('image')){
+            if ($file = $request->file('image')) {
                 $currentImage = $member->images()->first();
-                if($currentImage !== null){
-                    $this->deleteFile(str_replace("storage","public",$currentImage->path), $currentImage->name); // Deleting the image file
+                if ($currentImage !== null) {
+                    $this->deleteFile(str_replace("storage", "public", $currentImage->path), $currentImage->name); // Deleting the image file
                     $currentImage->delete();
                 }
                 $saved_image = $this->uploads($file, "public/uploads");
